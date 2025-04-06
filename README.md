700dc94f54d1b3d67ae7e9c341e172a863be1f97
CrystalW11 <crystal.warmack@gmail.com>
2025-04-06 17:28:04 -0600

    updated readme.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Conditional_Operator_Inline
 
 ![alt text](image.png)
+
+#### Output 
+
+![alt text](image-1.png)
--- obj/Debug/net9.0/12-Conditional_Operator_Inline.AssemblyInfo.cs
+++ obj/Debug/net9.0/12-Conditional_Operator_Inline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("12-Conditional_Operator_Inline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082cfe8f40b7ce6bc3cf24a81fe0599e67badbd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6639ce54c63e2ddf9e77831ce06838ce98c1ab6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("12-Conditional_Operator_Inline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("12-Conditional_Operator_Inline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
